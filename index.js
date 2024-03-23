b18675352fc0721fa8fcdbea4ffa42027ddf7c85
nedoratic <nedoratic@gmail.com>
2024-03-23 07:37:44 +0100

    window onload functionality added

---
+++ index.js
@@ -0,0 +1,10 @@
+// Window Onload Functionality
+window.onload = () => {
+	const container = document.getElementById('container');
+	const createLabel = (name) => {
+		const label = document.createElement('div');
+		label.className = 'label';
+		label.textContent = name;
+		container.appendChild(label);
+	};
+};
