a635ff0dc7c05cce9dcbba3add4e9151b139528a
nedoratic <nedoratic@gmail.com>
2024-03-25 09:43:01 +0100

    median function added

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
 // Average
 const average = (nums) => sum(nums) / nums.length;
 
+// Median
+const median = (nums) => {
+	const sorted = nums.slice().sort((a, b) => a - b);
+	const length = sorted.length;
+	const middle = length / 2 - 1;
+	return isEven(length) ? average([sorted[middle], sorted[middle + 1]]) : sorted[Math.ceil(middle)];
+};
+
 // Range
 const range = (start, end) =>
 	Array(end - start + 1)
