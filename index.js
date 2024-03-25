13081c19d51f6a7542ec66b51ff7297956ab2ada
nedoratic <nedoratic@gmail.com>
2024-03-25 09:39:34 +0100

    window onload functionality edited

--- index.js
+++ index.js
@@ -16,4 +16,16 @@ window.onload = () => {
 		label.textContent = name;
 		container.appendChild(label);
 	};
+	const letters = charRange('A', 'J');
+	letters.forEach(createLabel);
+	range(1, 99).forEach((number) => {
+		createLabel(number);
+		letters.forEach((letter) => {
+			const input = document.createElement('input');
+			input.type = 'text';
+			input.id = letter + number;
+			input.ariaLabel = letter + number;
+			container.appendChild(input);
+		});
+	});
 };
