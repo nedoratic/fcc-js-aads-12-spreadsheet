9594120820c8a36e9f5df8b4d27b8aea954ac028
nedoratic <nedoratic@gmail.com>
2024-03-26 20:20:03 +0100

    window on load functionality edited

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ window.onload = () => {
 			input.type = 'text';
 			input.id = letter + number;
 			input.ariaLabel = letter + number;
+			input.onchange = update;
 			container.appendChild(input);
 		});
 	});
