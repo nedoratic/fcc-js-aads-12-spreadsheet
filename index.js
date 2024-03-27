0eacb0844c9eb11cc536152dedfcd85bbec2bf52
nedoratic <nedoratic@gmail.com>
2024-03-27 20:30:06 +0100

    evaluate formula function added

--- index.js
+++ index.js
@@ -31,6 +31,18 @@ const range = (start, end) =>
 // Character Range
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code) => String.fromCharCode(code));
 
+// Eval Formula
+const evalFormula = (x, cells) => {
+	const idToText = (id) => cells.find((cell) => cell.id === id).value;
+	const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+	const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+	const elemValue = (num) => (character) => idToText(character + num);
+	const addCharacters = (character1) => (character2) => (num) => charRange(character1, character2).map(elemValue(num));
+	const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
+	const cellRegex = /[A-J][1-9][0-9]?/gi;
+	const cellExpanded = rangeExpanded.replace(cellRegex, (match) => idToText(match.toUpperCase()));
+};
+
 // Window Onload Functionality
 window.onload = () => {
 	const container = document.getElementById('container');
