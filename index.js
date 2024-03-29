d3e977557f03b615427e2a1e6c0ba527167ae1c4
nedoratic <nedoratic@gmail.com>
2024-03-29 19:54:21 +0100

    high precedence function added

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const infixToFunction = {
 // Infix Eval
 const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
 
+// High Precedence
+const highPrecedence = (str) => {
+	const regex = /([\d.]+)([*\/])([\d.]+)/;
+	const str2 = infixEval(str, regex);
+	return str === str2 ? str : highPrecedence(str2);
+};
+
 // Is Even
 const isEven = (num) => num % 2 === 0;
 
