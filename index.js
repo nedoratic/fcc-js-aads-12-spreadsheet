83955b204f8cad9f4104c473f83e3445106bdc88
nedoratic <nedoratic@gmail.com>
2024-04-01 21:51:04 +0200

    apply function added

--- index.js
+++ index.js
@@ -51,6 +51,17 @@ const spreadsheetFunctions = {
 	nodupes: (nums) => [...new Set(nums).values()],
 };
 
+// Apply Function
+const applyFunction = (str) => {
+	const noHigh = highPrecedence(str);
+	const infix = /([\d.]+)([+-])([\d.]+)/;
+	const str2 = infixEval(noHigh, infix);
+	const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+	const toNumberList = (args) => args.split(',').map(parseFloat);
+	const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+	return str2.replace(functionCall, (match, fn, args) => (spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match));
+};
+
 // Range
 const range = (start, end) =>
 	Array(end - start + 1)
